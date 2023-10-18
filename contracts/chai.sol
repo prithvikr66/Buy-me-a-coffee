b7d830c6e2f64d47efa8d6879c8eefac0c64ea3a
prithvikr66 <pkunofficial66@gmail.com>
2023-10-18 15:25:20 +0530

    initial commit

---
+++ contracts/chai.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract chai{
+    struct Memo{
+        string name;
+        string message;
+        uint256 setAt;
+        address fromAddress;
+    }
+
+    Memo[] memos;
+    address payable owner;
+    constructor(){
+        owner=payable(msg.sender);
+        }
+
+        function buyCoffee(string calldata name, string calldata message)external payable{
+            require(msg.value>1e18,"Please pay more than 0.1ETH");
+            owner.transfer(msg.value);
+            memos.push(Memo(name,message,block.timestamp,msg.sender));
+
+        }
+        function getMemos() public view returns(Memo[] memory){
+      return memos;
+        }
+}
